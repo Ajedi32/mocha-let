a7ba808805c5c13754222c4696a163aee2887eaa
Andrew Meyer <andrewm.bpi@gmail.com>
2015-12-19 15:03:04 -0600

    Create project skeleton

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.initConfig({
+    mochaTest: {
+      test: {
+        src: ['test.js']
+      }
+    },
+    jshint: {
+      code: ['Gruntfile.js', 'index.js'],
+      tests: ['test.js'],
+      options: {
+        jshintrc: true,
+      },
+    }
+  });
+
+  grunt.registerTask('test', 'mochaTest');
+  grunt.registerTask('lint', 'jshint');
+  grunt.registerTask('default', ['test', 'lint']);
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# mocha-let
+
+A simple package that lets you create RSpec-style memoized values in Mocha.
