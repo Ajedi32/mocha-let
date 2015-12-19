573e68252c868d450fee7764fbf1d49143b4e4c5
Andrew Meyer <andrewm.bpi@gmail.com>
2015-12-19 15:19:02 -0600

    Allow overriding previously set values

--- test.js
+++ test.js
@@ -5,6 +5,15 @@ describe('mocha-let', function() {
   var object = {};
   set('object', () => object);
 
+  context("in a sub-context", function() {
+    var aDifferentObject = {};
+    set('object', () => aDifferentObject);
+
+    it("allows overriding the value of an existing property", function() {
+      assert.equal(this.object, aDifferentObject);
+    });
+  });
+
   it("allows accessing the return value of the given function as the specified property on `this`", function() {
     assert.equal(this.object, object);
   });
